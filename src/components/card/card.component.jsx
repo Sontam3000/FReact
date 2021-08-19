d1afafe288bd321835f6fc950280261d0213c421
removedporn <86824510+removedporn@users.noreply.github.com>
2021-08-19 08:43:13 +0800

    Delete src directory

--- src/components/card/card.component.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import './card.component.css';
-
-export const Card = props=>(
-    <div className="card-container">
-        <img src={props.guitar.image} alt="guitar" height="280px;" width="170px"/>
-   <h2> {props.guitar.name}
-   </h2></div>
-   )
